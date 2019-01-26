7cf2451dc72087dbc2e96d0494af9d352683b288
Ray Soyama <raysoyama@gmail.com>
2019-01-25 23:09:02 -0800

    Added Trigger Materials, and started on PlayerManager (state manager)

---
+++ Assets/Homegame/Scripts/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int playerState;
+    public int armbandState;
+
+    public bool canWalk;
+    public bool canRun;
+    public bool canCrouch;
+    public bool canSidle;
+    public bool canKnock;
+    public bool canThrowRock;
+    public bool canTakeDown;
+
+
+    
+    private enum PLAYER_STATE
+    {
+        STILL,
+        WALK,
+        RUN,
+        CROUCH,
+        SIDLE,
+        KNOCK,
+        THROWROCK,
+        TAKEDOWN
+    }
+
+    private enum ARMBAND_STATE
+    {
+         OFF,
+         BLINK,
+         SOLID
+    }
+
+    void Start()
+    {
+
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
