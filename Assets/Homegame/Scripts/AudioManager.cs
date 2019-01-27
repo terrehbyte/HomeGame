f0dc08a9394a8af71f2e7a696e3b501f00a8e493
Ray Soyama <raysoyama@gmail.com>
2019-01-26 22:00:36 -0800

    Added Max's sound manager to the player prefab

---
+++ Assets/Homegame/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        AkBankManager.LoadBank("gameBank", true, true);
+        AkSoundEngine.SetState("attackState", "patrol");
+        AkSoundEngine.PostEvent("play_Music", gameObject);
+        //AkSoundEngine.SetRTPCValue("flatSharpParameter", pitchFlatten, soundObject);
+    }
+
+}
