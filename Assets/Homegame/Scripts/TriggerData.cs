fb2ac490a4e1da8a87a59f5358dc20d201334d95
Ray Soyama <raysoyama@gmail.com>
2019-01-27 01:06:23 -0800

    I added scripts for trigger detection, also added a hallway

---
+++ Assets/Homegame/Scripts/TriggerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerData : MonoBehaviour
+{
+    public Sprite button;
+    public string textUI;
+
+}
---
+++ Assets/Homegame/Scripts/TriggerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+    /*
+     * Handles fetching Data from the trigger object, and then sending it to the UI
+     * <3 
+     * 
+     */
+
+
+    private bool shitChanged;
+    private string text;
+    private Collider lastCollider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shitChanged == true)
+        {
+            shitChanged = false;
+            Debug.Log("TEXT: " + text);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger") && lastCollider != other)
+        {
+            lastCollider = other;
+            shitChanged = true;
+            text = other.gameObject.GetComponent<TriggerData>().textUI;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            lastCollider = null;
+            text = "";
+            shitChanged = true;
+        }
+    }
+
+}
