3379f4cfdb49207960bc1516d4d7f16f025f4dc0
Terry Nguyen <terreh@terrehbyte.com>
2019-01-27 17:59:53 -0800

    Add generic trigger

---
+++ Assets/Homegame/Scripts/GenericTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericTrigger : MonoBehaviour
+{
+    public UnityEventTrigger OnTriggerEntered;
+    public UnityEventTrigger OnTriggerStayed;
+    public UnityEventTrigger OnTriggerExited;
+
+    void OnTriggerStart(Collider coll)
+    {
+        OnTriggerEntered.Invoke(coll, TriggerType.START);
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        OnTriggerStayed.Invoke(coll, TriggerType.STAY);
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        OnTriggerExited.Invoke(coll, TriggerType.EXIT);
+    }
+}
+
+public enum TriggerType
+{
+    START,
+    STAY,
+    EXIT
+}
+
+[System.Serializable]
+public class UnityEventTrigger : UnityEvent<Collider, TriggerType> {}
